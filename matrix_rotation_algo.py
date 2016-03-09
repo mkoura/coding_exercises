d406b3d21134c10bd0a07854852128508f8f8cd8
Martin Kourim <martin@kourim.net>
2016-03-10 00:33:11 +0100

    simplify matrix rotation algo

--- matrix_rotation_algo.py
+++ matrix_rotation_algo.py
@@ -10,33 +10,37 @@ for i in range(M):
     matrix.append(list(input().strip().split(' ')))
 nestings = min((int(M / 2) + int(M % 2), int(N / 2) + int(N % 2)))
 
-layered_matrix = []
-for i in range(nestings):
-    last_c = N - i - 1
-    last_r = M - i - 1
-    tmp = matrix[i][i:last_c]
-    for j in range(i, last_r):
-        tmp.append(matrix[j][last_c])
-    tmp.extend(reversed(matrix[last_r][i + 1:last_c + 1]))
-    for k in range(last_r, i, -1):
-        tmp.append(matrix[k][i])
-    layered_matrix.append(tmp)
-
 rotated_lists = []
-for m in layered_matrix:
-    l = len(m)
-    rot = R % l
-    rotated_lists.append(m[rot:] + m[:rot])
+for i in range(nestings):
+    last_c = N - i - 1
+    last_r = M - i - 1
+    # left - right
+    crow = matrix[i][i:last_c]
+    # right - bottom
+    for j in range(i, last_r):
+        crow.append(matrix[j][last_c])
+    # bottom - left
+    for j in range(last_c, i, -1):
+        crow.append(matrix[last_r][j])
+    # left - top
+    for j in range(last_r, i, -1):
+        crow.append(matrix[j][i])
+    rot = R % len(crow)
+    rotated_lists.append(crow[rot:] + crow[:rot])
 
 for i in range(nestings):
     last_c = N - i - 1
     last_r = M - i - 1
+    # left - bottom
     for j in range(i + 1, last_r):
         matrix[j][i] = rotated_lists[i].pop()
+    # bottom - right
     for j in range(i, last_c):
         matrix[last_r][j] = rotated_lists[i].pop()
+    # right - top
     for j in range(last_r, i, -1):
         matrix[j][last_c] = rotated_lists[i].pop()
+    # top - left
     for j in range(last_c, i - 1, -1):
         matrix[i][j] = rotated_lists[i].pop()
 
